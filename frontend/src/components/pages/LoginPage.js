11dbbe13582778f1736829627c5115c69640cae5
User <yunus.asadulaev@mil.ru>
2023-04-24 02:07:34 +0300

    bootstrap to react-bootstrap

--- frontend/src/components/pages/LoginPage.js
+++ frontend/src/components/pages/LoginPage.js
@@ -1,82 +1,126 @@
-﻿import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+﻿import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { Container, Row, Col, Form, Button, Card } from 'react-bootstrap';
 
-export default function SignInPage() {
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
+export default function LoginPage() {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		axios.post('http://localhost:5000/login', { email, password }, {
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-		.then((response) => {
-			console.log('Успешная авторизация:', response.data)
-				// здесь можно добавить код для перенаправления на другую страницу или для обновления состояния приложения
-		})
-		.catch((error) => {
-			console.error('Ошибка авторизации:', error)
-			// здесь можно добавить код для обработки ошибок, например, для вывода сообщения об ошибке на экране
-		})
-	}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post(
+                'http://localhost:5000/login',
+                { email, password },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            )
+            .then((response) => {
+                console.log('Успешная авторизация:', response.data);
+                // здесь можно добавить код для перенаправления на другую страницу или для обновления состояния приложения
+            })
+            .catch((error) => {
+                console.error('Ошибка авторизации:', error);
+                // здесь можно добавить код для обработки ошибок, например, для вывода сообщения об ошибке на экране
+            });
+    };
 
     return (
-		<section class="h-100">
-			<div class="container h-100">
-				<div class="row justify-content-sm-center h-100">
-					<div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
-						<div class="text-center my-5">
-							<img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="logo" width="100" />
-						</div>
-						<div class="card shadow-lg">
-							<div class="card-body p-5">
-								<h1 class="fs-4 card-title fw-bold mb-4">Вход</h1>
-								<form method="POST" onSubmit={handleSubmit} class="needs-validation" novalidate="" autocomplete="off">
-									<div class="mb-3">
-										<label class="mb-2 text-muted" for="email">Email</label>
-										<input id="email" type="email" class="form-control" name="email" required autofocus onChange={(e) => setEmail(e.target.value)} />
-											<div class="invalid-feedback">
-												Неверная почта.
-											</div>
-									</div>
+        <section className="h-100">
+            <Container className="h-100">
+                <Row className="justify-content-sm-center h-100">
+                    <Col
+                        xxl={4}
+                        xl={5}
+                        lg={5}
+                        md={7}
+                        sm={9}
+                        className="d-flex flex-column justify-content-between"
+                    >
+                        <div className="text-center my-5">
+                            <img
+                                src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg"
+                                alt="logo"
+                                width="100"
+                            />
+                        </div>
+                        <Card className="shadow-lg">
+                            <Card.Body className="p-5">
+                                <h1 className="fs-4 card-title fw-bold mb-4">Вход</h1>
+                                <Form
+                                    onSubmit={handleSubmit}
+                                    className="needs-validation"
+                                    noValidate
+                                    autoComplete="off"
+                                >
+                                    <Form.Group controlId="email" className="mb-3">
+                                        <Form.Label className="mb-2 text-muted">
+                                            Email
+                                        </Form.Label>
+                                        <Form.Control
+                                            type="email"
+                                            name="email"
+                                            required
+                                            autoFocus
+                                            value={email}
+                                            onChange={(e) => setEmail(e.target.value)}
+                                            isInvalid={false} // здесь можно добавить логику для отображения ошибки
+                                        />
+                                        <Form.Control.Feedback type="invalid">
+                                            Неверная почта.
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
 
-									<div class="mb-3">
-										<div class="mb-2 w-100">
-											<label class="text-muted" for="password">Пароль</label>
-											<Link to="" class="float-end link-primary">Забыли пароль?</Link>
-										</div>
-										<input id="password" type="password" class="form-control" name="password" required onChange={(e) => setPassword(e.target.value)} />
-											<div class="invalid-feedback">
-												Неверный пароль.
-											</div>
-									</div>
+                                    <Form.Group controlId="password" className="mb-3">
+                                        <div className="mb-2 w-100 d-flex justify-content-between align-items-center">
+                                            <Form.Label className="text-muted">
+                                                Пароль
+                                            </Form.Label>
+                                            <Link to="" className="link-primary">
+                                                Забыли пароль?
+                                            </Link>
+                                        </div>
+                                        <Form.Control
+                                            type="password"
+                                            name="password"
+                                            required
+                                            value={password}
+                                            onChange={(e) => setPassword(e.target.value)}
+                                            isInvalid={false} // здесь можно добавить логику для отображения ошибки
+                                        />
+                                        <Form.Control.Feedback type="invalid">
+                                            Неверный пароль.
+                                        </Form.Control.Feedback>
+                                    </Form.Group>
 
-									<div class="d-flex align-items-center">
-										<div class="form-check">
-											<input type="checkbox" name="remember" id="remember" class="form-check-input" />
-												<label for="remember" class="form-check-label">Запомнить меня</label>
-										</div>
-										<button type="submit" class="btn btn-primary ms-auto">
-											Войти
-										</button>
-									</div>
-								</form>
-							</div>
-							<div class="card-footer py-3 border-0">
-								<div class="text-center">
-									Нет аккаунта? <Link to="/register" class="text-dark">Создать</Link>
-								</div>
-							</div>
-						</div>
-						<div class="text-center mt-5 text-muted">
-							Copyright &copy; 2023-2100 &mdash; Find My Gym
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-    )
+                                    <Form.Group controlId="remember" className="mb-3">
+                                        <Form.Check type="checkbox" label="Запомнить меня" />
+                                    </Form.Group>
+
+                                    <Button type="submit" variant="primary" className="ms-auto">
+                                        Войти
+                                    </Button>
+                                </Form>
+                            </Card.Body>
+                            <Card.Footer className="py-3 border-0">
+                                <div className="text-center">
+                                    Нет аккаунта?{' '}
+                                    <Link to="/register" className="text-dark">
+                                        Создать
+                                    </Link>
+                                </div>
+                            </Card.Footer>
+                        </Card>
+                        <div className="text-center mt-5 text-muted">
+                            &copy; 2023-2100 &mdash; Find My Gym
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
 }
--- frontend/src/components/pages/RegisterPage.js
+++ frontend/src/components/pages/RegisterPage.js
@@ -1,129 +1,193 @@
-﻿import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
+﻿import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import {
+    Container,
+    Row,
+    Col,
+    Card,
+    Form,
+    FormGroup,
+    FormLabel,
+    FormControl,
+    FormText,
+    FormSelect,
+    Button,
+} from 'react-bootstrap';
 
 export default function SignUpPage() {
-	const [first_name, setName] = useState('')
-	const [last_name, setSurname] = useState('')
-	const [role, setRole] = useState('Пользователь')
-	const [user, setUser] = useState(true)
-	const [coach, setCoach] = useState(false)
-	const [phone, setPhone] = useState('')
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
+    const [first_name, setName] = useState('');
+    const [last_name, setSurname] = useState('');
+    const [role, setRole] = useState('Пользователь');
+    const [user, setUser] = useState(true);
+    const [coach, setCoach] = useState(false);
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
-	const handleSubmit = (e) => {
-		e.preventDefault()
-		axios.post('http://localhost:5000/register', { first_name, last_name, phone, email, password, user, coach }, {
-			headers: {
-				'Content-Type': 'application/json'
-			}
-		})
-		.then((response) => {
-			console.log('Успешная регистрация:', response.data)
-			// здесь можно добавить код для перенаправления на другую страницу или для обновления состояния приложения
-		})
-		.catch((error) => {
-			console.error('Ошибка регистрации:', error)
-			// здесь можно добавить код для обработки ошибок, например, для вывода сообщения об ошибке на экране
-		})
-	}
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios
+            .post(
+                'http://localhost:5000/register',
+                { first_name, last_name, phone, email, password, user, coach },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            )
+            .then((response) => {
+                console.log('Успешная регистрация:', response.data);
+                // здесь можно добавить код для перенаправления на другую страницу или для обновления состояния приложения
+            })
+            .catch((error) => {
+                console.error('Ошибка регистрации:', error);
+                // здесь можно добавить код для обработки ошибок, например, для вывода сообщения об ошибке на экране
+            });
+    };
 
-	const handleRoleChange = (e) => {
-		setRole(e.target.value)
-		if (e.target.value === 'Пользователь') {
-			setUser(true)
-			setCoach(false)
-		} else if (e.target.value === 'Тренер') {
-			setUser(false)
-			setCoach(true)
-		}
-	}
+    const handleRoleChange = (e) => {
+        setRole(e.target.value);
+        if (e.target.value === 'Пользователь') {
+            setUser(true);
+            setCoach(false);
+        } else if (e.target.value === 'Тренер') {
+            setUser(false);
+            setCoach(true);
+        }
+    };
 
     return (
-		<section class="h-100">
-			<div class="container h-100">
-				<div class="row justify-content-sm-center h-100">
-					<div class="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
-						<div class="text-center my-5">
-							<img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="logo" width="100" />
-						</div>
-						<div class="card shadow-lg">
-							<div class="card-body p-5">
-								<h1 class="fs-4 card-title fw-bold mb-4">Регистрация</h1>
-								<form method="POST" onSubmit={handleSubmit} class="needs-validation" novalidate="" autocomplete="off">
-									<div class="mb-3">
-										<label class="mb-2 text-muted" for="name">Имя</label>
-										<input id="first_name" type="text" class="form-control" name="first_name" required autofocus onChange={(e) => setName(e.target.value)} />
-											<div class="invalid-feedback">
-												Обязательное поле
-											</div>
-									</div>
+        <section className="h-100">
+            <Container className="h-100">
+                <Row className="justify-content-sm-center h-100">
+                    <Col xxl={4} xl={5} lg={5} md={7} sm={9}>
+                        <div className="text-center my-5">
+                            <img
+                                src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg"
+                                alt="logo"
+                                width="100"
+                            />
+                        </div>
+                        <Card className="shadow-lg">
+                            <Card.Body className="p-5">
+                                <h1 className="fs-4 card-title fw-bold mb-4">Регистрация</h1>
+                                <Form
+                                    method="POST"
+                                    onSubmit={handleSubmit}
+                                    className="needs-validation"
+                                    noValidate=""
+                                    autoComplete="off"
+                                >
+                                    <FormGroup className="mb-3">
+                                        <FormLabel className="mb-2 text-muted" htmlFor="first_name">
+                                            Имя
+                                        </FormLabel>
+                                        <FormControl
+                                            id="first_name"
+                                            type="text"
+                                            name="first_name"
+                                            required
+                                            autoFocus
+                                            onChange={(e) => setName(e.target.value)}
+                                        />
+                                        <div className="invalid-feedback">Обязательное поле</div>
+                                    </FormGroup>
 
-									<div class="mb-3">
-										<label class="mb-2 text-muted" for="name">Фамилия</label>
-										<input id="last_name" type="text" class="form-control" name="last_name" required onChange={(e) => setSurname(e.target.value)} />
-										<div class="invalid-feedback">
-											Обязательное поле
-										</div>
-									</div>
+                                    <FormGroup className="mb-3">
+                                        <FormLabel className="mb-2 text-muted" htmlFor="last_name">
+                                            Фамилия
+                                        </FormLabel>
+                                        <FormControl
+                                            id="last_name"
+                                            type="text"
+                                            name="last_name"
+                                            required
+                                            onChange={(e) => setSurname(e.target.value)}
+                                        />
+                                        <div className="invalid-feedback">Обязательное поле</div>
+                                    </FormGroup>
 
-									<div class="mb-3">
-										<label class="mb-2 text-muted">Роль</label>
-										<select class="form-select" aria-label="Default select example" onChange={ handleRoleChange }>
-											<option selected>Пользователь</option>
-											<option value="1">Тренер</option>
-										</select>
-									</div> 
+                                    <FormGroup className="mb-3">
+                                        <FormLabel className="mb-2 text-muted">Роль</FormLabel>
+                                        <FormSelect
+                                            aria-label="Default select example"
+                                            onChange={handleRoleChange}
+                                        >
+                                            <option selected>Пользователь</option>
+                                            <option value="1">Тренер</option>
+                                        </FormSelect>
+                                    </FormGroup>
 
-									<div class="mb-3">
-										<label class="mb-2 text-muted" for="phone">Телефон</label>
-										<input id="phone" type="tel" class="form-control" name="phone" required onChange={(e) => setPhone(e.target.value)} />
-										<div class="invalid-feedback">
-											Обязательное поле
-										</div>
-									</div>
+                                    <FormGroup className="mb-3">
+                                        <FormLabel className="mb-2 text-muted" htmlFor="phone">
+                                            Телефон
+                                        </FormLabel>
+                                        <FormControl
+                                            id="phone"
+                                            type="tel"
+                                            name="phone"
+                                            required
+                                            onChange={(e) => setPhone(e.target.value)}
+                                        />
+                                        <div className="invalid-feedback">Обязательное поле</div>
+                                    </FormGroup>
 
-									<div class="mb-3">
-										<label class="mb-2 text-muted" for="email">Email</label>
-										<input id="email" type="email" class="form-control" name="email" required onChange={(e) => setEmail(e.target.value)} />
-											<div class="invalid-feedback">
-												Обязательное поле
-											</div>
-									</div>
+                                    <FormGroup className="mb-3">
+                                        <FormLabel className="mb-2 text-muted" htmlFor="email">
+                                            Email
+                                        </FormLabel>
+                                        <FormControl
+                                            id="email"
+                                            type="email"
+                                            name="email"
+                                            required
+                                            onChange={(e) => setEmail(e.target.value)}
+                                        />
+                                        <div className="invalid-feedback">Обязательное поле</div>
+                                    </FormGroup>
 
-									<div class="mb-3">
-										<label class="mb-2 text-muted" for="password">Пароль</label>
-										<input id="password" type="password" class="form-control" name="password" required onChange={(e) => setPassword(e.target.value)} />
-											<div class="invalid-feedback">
-												Обязательное поле
-											</div>
-									</div>
+                                    <FormGroup className="mb-3">
+                                        <FormLabel className="mb-2 text-muted" htmlFor="password">
+                                            Пароль
+                                        </FormLabel>
+                                        <FormControl
+                                            id="password"
+                                            type="password"
+                                            name="password"
+                                            required
+                                            onChange={(e) => setPassword(e.target.value)}
+                                        />
+                                        <div className="invalid-feedback">Обязательное поле</div>
+                                    </FormGroup>
 
-									<p class="form-text text-muted mb-3">
-										Нажимая "Отправить" Вы соглашаетесь с нашей политикой конфиденциальности.
-									</p>
-
-									<div class="align-items-center d-flex">
-										<button type="submit" class="btn btn-primary ms-auto">
-											Отправить
-										</button>
-									</div>
-								</form>
-							</div>
-							<div class="card-footer py-3 border-0">
-								<div class="text-center">
-									Уже есть аккаунт? <Link to="/login" class="text-dark">Войти</Link>
-								</div>
-							</div>
-						</div>
-						<div class="text-center mt-5 text-muted">
-							Copyright &copy; 2023-2100 &mdash; Find My Gym
-						</div>
-					</div>
-				</div>
-			</div>
-		</section>
-    )
-
+                                    <Form.Group className="mb-3">
+                                        <Form.Text className="text-muted">
+                                            Нажимая "Отправить" Вы соглашаетесь с нашей политикой конфиденциальности.
+                                        </Form.Text>
+                                    </Form.Group>
+                                    <Row className="justify-content-start">
+                                        <Col xs="auto">
+                                            <Button type="submit" variant="primary">
+                                                Отправить
+                                            </Button>
+                                        </Col>
+                                    </Row>
+                                </Form>
+                            </Card.Body>
+                            <Card.Footer className="py-3 border-0">
+                                <div className="text-center">
+                                    Уже есть аккаунт? <Link to="/login" className="text-dark">Войти</Link>
+                                </div>
+                            </Card.Footer>
+                        </Card>
+                        <div className="text-center mt-5 text-muted">
+                            Copyright &copy; 2023-2100 &mdash; Find My Gym
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
 }
